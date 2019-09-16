73f386f4ed1a87bd4a797933953e305065371f5f
Robert Gulewicz <nayael@wp.pl>
2019-09-17 00:17:35 +0200

    Extend model

--- src/models/Card.ts
+++ src/models/Card.ts
@@ -1,6 +1,25 @@
 export interface Card {
     id: string;
-    name: string;
     imageUrl: string;
     imageUrlHiRes: string;
+    name: string;
+    supertype: string;
+    series: string;
+    types: string[];
+    rarity: string;
+    nationalPokedexNumber: number;
+    hp: string;
+    set: string;
+    evolvesFrom: string;
+    weaknesses: {
+        type: string;
+        value: string
+    }[];
+    attacks: {
+        cost: string[];
+        name: string;
+        text: string;
+        damage: string;
+        convertedEnergyCost: number;
+    }[];
 }
