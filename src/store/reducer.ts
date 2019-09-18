95b3d734eff8cb995edde6e4c538c21afbc7bdac
Robert Gulewicz <nayael@wp.pl>
2019-09-18 02:19:44 +0200

    Implement remaining actions

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -9,7 +9,7 @@ function reducer(state: State = initialState, action: Action): State {
             return {
                 ...state,
                 pendingRequests: {
-                    ...state.pendingRequests,
+                    ...state.pendingRequests, // TODO: check if last batch has been retrieved
                     cardList: true
                 }
             };
@@ -37,9 +37,114 @@ function reducer(state: State = initialState, action: Action): State {
                     ...state.cardListIDs,
                     ...newCardIds
                 ]
-                // cards: action.payload
             };
         }
+
+        case getType(actions.requestCardBatchFailure): {
+            return {
+                ...state,
+                pendingRequests: {
+                    ...state.pendingRequests,
+                    cardList: false
+                },
+                errorMessage: action.payload
+            };
+        }
+
+        case getType(actions.openCardDetails): {
+            const id = action.payload;
+            const cardExists = state.cards.hasOwnProperty(id);
+            const similarCardsExist = state.similarCardIDs.hasOwnProperty(id);
+
+            return {
+                ...state,
+                pendingRequests: {
+                    ...state.pendingRequests,
+                    selectedCard: !cardExists,
+                    similarCards: !similarCardsExist
+                },
+                selectedCardID: id
+            };
+        }
+
+        case getType(actions.closeCardDetails): {
+            return {
+                ...state,
+                pendingRequests: {
+                    ...state.pendingRequests,
+                    selectedCard: false,
+                    similarCards: false
+                },
+                selectedCardID: null
+
+            }
+        }
+
+        case getType(actions.fetchCardDetailsSuccess): {
+            const newCard = action.payload;
+            return {
+                ...state,
+                cards: {
+                    [newCard.id]: newCard,
+                    ...state.cards  
+                },
+                pendingRequests: {
+                    ...state.pendingRequests,
+                    selectedCard: false,
+                    similarCards: false
+                },
+            }
+        }
+
+        case getType(actions.fetchCardDetailsFailure): {
+            return {
+                ...state,
+                pendingRequests: {
+                    ...state.pendingRequests,
+                    selectedCard: false,
+                    similarCards: false
+                },
+                errorMessage: action.payload
+            };
+        }
+
+        case getType(actions.fetchSimilarCardsSuccess): {
+            const baseCardId = action.payload.card.id;
+            const similarCards = action.payload.similarCards.reduce((cards, card) => ({
+                ...cards,
+                [card.id]: card
+            }), {});
+
+            const similarCardIDs = action.payload.similarCards.map(card => card.id);
+
+            return {
+                ...state,
+                cards: {
+                    ...similarCards,
+                    ...state.cards  
+                },
+                similarCardIDs: {
+                    ...state.similarCardIDs,
+                    [baseCardId]: similarCardIDs
+                },
+                pendingRequests: {
+                    ...state.pendingRequests,
+                    similarCards: false
+                },
+            }
+        }
+
+        case getType(actions.fetchSimilarCardsFailure): {
+            return {
+                ...state,
+                pendingRequests: {
+                    ...state.pendingRequests,
+                    similarCards: false
+                },
+                errorMessage: action.payload
+            };
+        }
+
     }
 
     return state;
