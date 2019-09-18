2148a586dd7c343ddac70dd46c531da4bf1a5f6c
Robert Gulewicz <nayael@wp.pl>
2019-09-18 02:19:19 +0200

    Add more actions

--- src/store/actions/actions.ts
+++ src/store/actions/actions.ts
@@ -19,3 +19,44 @@ export const requestCardBatchFailure = createStandardAction('REQUEST_CARD_BATCH_
 );
 
 export const lastCardBatchRetrieved = createStandardAction('LAST_CARD_BATCH_RETRIEVED')();
+
+export const openCardDetails = createStandardAction('OPEN_CARD_DETAILS').map(
+    (cardId: string) => ({
+        payload: cardId
+    })
+);
+
+export const closeCardDetails = createStandardAction('CLOSE_CARD_DETAILS')();
+
+export const fetchCardDetailsSuccess = createStandardAction('FETCH_CARD_DETAILS_SUCCESS').map(
+    (card: Card) => ({
+        payload: card
+    })
+);
+
+export const fetchCardDetailsFailure = createStandardAction('FETCH_CARD_DETAILS_FAILURE').map(
+    (errorMessage: string) => ({
+        payload: errorMessage
+    })
+);
+
+export const fetchSimilarCards = createStandardAction('FETCH_SIMILAR_CARDS').map(
+    (card: Card) => ({
+        payload: card
+    })
+);
+
+export const fetchSimilarCardsSuccess = createStandardAction('FETCH_SIMILAR_CARDS_SUCCESS').map(
+    ({ card, similarCards }: { card: Card, similarCards: Card[] }) => ({
+        payload: {
+            card,
+            similarCards
+        }
+    })
+);
+
+export const fetchSimilarCardsFailure = createStandardAction('FETCH_SIMILAR_CARDS_FAILURE').map(
+    (errorMessage: string) => ({
+        payload: errorMessage
+    })
+);
