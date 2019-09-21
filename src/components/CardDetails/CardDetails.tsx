0e2b8c2187345da1dbce843fae60da2b423675ad
Robert Gulewicz <nayael@wp.pl>
2019-09-22 01:58:34 +0200

    Display card properties

--- src/components/CardDetails/CardDetails.tsx
+++ src/components/CardDetails/CardDetails.tsx
@@ -1,13 +1,68 @@
 import React from 'react';
 
 import { Card } from '../../models';
-import { Details } from './CardDetails.styles';
+import {
+    Details,
+    Image,
+    Properties,
+    Property,
+    PropertyName,
+    TopWrapper,
+    Weakness,
+    Weaknesses
+} from './CardDetails.styles';
 
 interface CardDetailsProps extends Card {}
 
-const CardDetails: React.FC<CardDetailsProps> = ({ name }) => (
+const CardDetails: React.FC<CardDetailsProps> = ({
+    id,
+    name,
+    supertype,
+    series,
+    types,
+    rarity,
+    nationalPokedexNumber,
+    hp,
+    set,
+    weaknesses,
+    attacks,
+    evolvesFrom,
+    imageUrlHiRes
+}) => (
     <Details>
-        {name}
+        <TopWrapper>
+            <Image src={imageUrlHiRes}/>
+            <Properties>
+                <PropertyName>Name</PropertyName>
+                <Property>{name}</Property>
+                <PropertyName>Supertype</PropertyName>
+                <Property>{supertype}</Property>
+                <PropertyName>Identifier</PropertyName>
+                <Property>{id}</Property>
+                <PropertyName>Series</PropertyName>
+                <Property>{series}</Property>
+                <PropertyName>Types</PropertyName>
+                <Property>{types.join(', ')}</Property>
+                <PropertyName>Rarity</PropertyName>
+                <Property>{rarity}</Property>
+                <PropertyName>National Pokédex number</PropertyName>
+                <Property>{nationalPokedexNumber}</Property>
+                <PropertyName>HP</PropertyName>
+                <Property>{hp}</Property>
+                <PropertyName>Set</PropertyName>
+                <Property>{set}</Property>
+                {evolvesFrom && <PropertyName>Evolves from</PropertyName>}
+                {evolvesFrom && <Property>{evolvesFrom}</Property>}
+                <PropertyName>{weaknesses.length > 1 ? 'Weaknesses' : 'Weakness'}</PropertyName>
+                <Property>
+                    <Weaknesses>
+                        {weaknesses.map(({ type, value }) => <Weakness key={type}>{`${type}: ${value}`}</Weakness>)}
+                    </Weaknesses>
+                </Property>
+            </Properties>
+        </TopWrapper>
+    
+
     </Details>
 );
 
